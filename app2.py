d1195600cb3597345ffa0fcc18518cff025e21ed
s200036 <gimpo.s200036@ggh.goe.go.kr>
2024-10-30 01:24:54 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 #  기계학습 모델 파일 로드(모델명 : logistic_regression_model.pkl)
-import joblib
-model = joblib.load('logistic_regression_model.pkl')
+#import joblib
+#model = joblib.load('logistic_regression_model.pkl')
 
 # 만든 모델로 테스트 데이터에 대해 예측하기
 st.title('합불 분류 지능에이전트')
